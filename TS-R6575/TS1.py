e52c4f95a32acad83bd6d31c65ff57b42c87b3d3
D8aCruz <david8acruz@gmail.com>
2023-08-19 00:02:15 -0300

    Tarea semanal funcionando correctamente

--- TS-R6575/TS1.py
+++ TS-R6575/TS1.py
@@ -1,35 +1,66 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Aug 18 18:49:54 2023
-
-@author: david
-"""
-
 import numpy as np
 import matplotlib.pyplot as plt
+from IPython.display import display, Image
 from scipy import signal
 
 # Parámetros de la Simulación
-fs = 250    #   Hz
-N  = fs     #   Cantidad de muestras digitalizada por el ADC (# muestras)
-Ts = 1/fs
-T_simulacion = N*Ts     # 1 Segundo
+fs = 500                #   Hz
+N  = fs                 #   Cantidad de muestras digitalizadas por el ADC (# muestras)
+Ts = 1/fs               #   Periodo de muestreo
+T_simulacion = N*Ts     #   1 Segundo
+
+def plot_signal(tt, signal, title):
+    plt.figure(figsize=(12, 4))
+    plt.plot(tt, signal)
+    plt.title(title, color='gray')
+    plt.xlabel('tt (s)')
+    plt.ylabel('Amplitud')
+    plt.grid(True, linestyle='dotted', color='gray')
+    plt.savefig(f'{title}.png', bbox_inches='tight', transparent=True)
+    plt.close()
+    
+def mi_funcion_sen(vmax = 1, dc = 0, ff = 1, ph = 0, nn = N, fs = fs):
+    # Grilla Temporal
+    tt = np.arange(start=0, stop=T_simulacion, step=Ts)
+    xx = vmax * np.sin(2 * np.pi * ff * tt + ph) + dc
+    return tt, xx
+def mi_funcion_cuadrada(vmax = 1, dc = 0, ff = 1, duty = 0.5, nn = N, fs = fs):
+    tt = np.arange(start=0, stop=T_simulacion, step=Ts)
+    xx = vmax*signal.square((2 * np.pi * ff * tt),duty) + dc
+    return tt, xx
+
+def mi_funcion_triangular(vmax = 1, dc = 0, ff = 1, width = 0.5, nn = N, fs = fs):
+    tt = np.arange(start=0, stop=T_simulacion, step=Ts)
+    xx = vmax*signal.sawtooth((2 * np.pi * ff * tt),width) + dc
+    return tt, xx
+
+
 
 # Parametros de la señal
-frecuencia  = 10    # Hz
-Amplitud    = 2     # V
-
-# Grilla Temporal
-tiempo = np.arange(start = 0, stop = T_simulacion,step = Ts)
+frecuencia    = 10        # Hz
+Amplitud      = 2         # V
+Phase         = 0*np.pi/2.0 # Radianes
+ValorMedio    = 0         # V
+Duty          = 0.5       # 50% de ciclo de actividad
+Width         = 0.5           
 
 # Señales periódicas
-SignalSen = Amplitud*np.sin(2*np.pi*frecuencia*tiempo)
-SignalCuad = Amplitud*signal.square((2*np.pi*frecuencia*tiempo), 0.5)
-SignalTria = Amplitud*signal.sawtooth((2*np.pi*frecuencia*tiempo), 0.5)
-# Graficamos la señal
-plt.style.use('dark_background')
-plt.plot(tiempo, SignalSen)
-plt.plot(tiempo, SignalCuad)
-plt.plot(tiempo, SignalTria)
+tiempo, SignalSen = mi_funcion_sen(vmax=Amplitud, ff=frecuencia, dc=ValorMedio, ph=Phase)
+tiempo, SignalCua = mi_funcion_cuadrada(vmax=Amplitud, ff=frecuencia, dc=ValorMedio, duty=Duty)
+tiempo, SignalTri = mi_funcion_triangular(vmax=Amplitud, ff=frecuencia, dc=ValorMedio, width=Width)
 
+# Configurar estilo global
+plt.rcParams['savefig.transparent'] = True
+plt.rcParams['axes.edgecolor'] = 'gray'
+plt.rcParams['axes.labelcolor'] = 'gray'
+plt.rcParams['xtick.color'] = 'gray'
+plt.rcParams['ytick.color'] = 'gray'
 
+# Generar y guardar figuras
+plot_signal(tiempo, SignalSen, 'Señal Senoidal')
+plot_signal(tiempo, SignalCua, 'Señal Cuadrada')
+plot_signal(tiempo, SignalTri, 'Señal Triangular')
+
+# Mostrar las imágenes en el notebook
+for title in ['Señal Senoidal', 'Señal Cuadrada', 'Señal Triangular']:
+    display(Image(filename=f'{title}.png'))
